9c48f0123adc244e3d948068c7e36102f544c665
AchRizalAdi <adijr979@gmail.com>
2025-01-31 18:31:47 +0700

    Ping and aransement something idk

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ DISCORD_CLIENT_ID=
 DISCORD_GUILD_ID=
 
 then you good to go
+
+command
+node . // to start
+node deploy-commands.js // to refresh adjustment
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -2,8 +2,12 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder().setName("ping").setDescription("Halo sayanmg"),
-  async execute(interaction) {
+  async execute(message, args) {
     // console.log(interaction, 'kau ni ape')
-    await interaction.reply("Pantek kau ni!");
+    // await interaction.reply("Pantek kau ni!");
+    // console.log(interaction, 'nah ndi')
+    await message.reply(`🏓Latency is ${Date.now() - message.createdTimestamp}ms`);
+    // const sent = await interaction.reply({ content: "Pinging..." });
+    // interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
 };
